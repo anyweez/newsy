ab7412774a491332ded0f963c5eeb25122f33c70
Luke Segars <luke@lukesegars.com>
2017-01-25 00:55:53 -0500

    Restructuring ArticleMetadata to include more practical info.

--- newsy.py
+++ newsy.py
@@ -9,28 +9,28 @@ md = rethinkdb.connect('historian', 28015)
 # rethinkdb
 
 class ArticleMetadata(object):
-    def __init__(self, article, path):
+    def __init__(self, article, source):
         h = hashlib.sha256()
         h.update(article.title.encode('utf-8'))
 
         self.id = h.hexdigest()
         self.title = article.title
         self.authors = article.authors
-        self.source = None
+        self.source = source['source']
+        self.url = source['url']
         
         self.published = article.publish_date.isoformat() if article.publish_date else None
         self.added = datetime.datetime.now().isoformat()
-        self.filepath = path
 
 
 ## Read, parse, and extract information from the specified HTML file.
-def extract(filename):
+def extract(filename, source):
     article = newspaper.Article(DOWNLOAD_URL_ROOT + filename)
 
     article.download()
     article.parse()
     
-    return ArticleMetadata(article, NEWS_DIRECTORY + filename), article.text
+    return ArticleMetadata(article, source), article.text
 
 def writeContent(filename, content):
     with open(filename + '.txt', 'w') as fp:
@@ -45,13 +45,15 @@ articles = []
 with open(NEWS_DIRECTORY[:-1] + '.json') as fp:
    articles = json.loads(fp.read())['records']
 
-## Iterate over each, one at a time, and parse
-for article in articles[:5]:
+## Iterate over each, one at a time, and parse. Create metadata records for
+## each story.
+for article in articles:
     filename = '{}.html'.format(article['_id'])
 
     try:
-        metadata, content = extract(filename)
+        metadata, content = extract(filename, article)
         writeContent(NEWS_DIRECTORY + filename, content)
         writeRecord(metadata)
-    except:
+    except Exception as e:
         print('Difficulty parsing article ' + article['_id'])
+        print(e)

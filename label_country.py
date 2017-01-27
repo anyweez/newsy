5c581eb6bfe424055a644357b0e4e130a20008c1
Luke Segars <luke@lukesegars.com>
2017-01-26 21:53:37 -0500

    Changed label format.

--- label_country.py
+++ label_country.py
@@ -1,6 +1,5 @@
 import rethinkdb, sys, nltk, os
 import static.countries as static
-# from nltk.text import TextCollection
 
 NEWS_FILE_PATH = os.environ['HOME'] + '/git/scraper/news/{}.html.txt'
 
@@ -11,11 +10,10 @@ def next_record():
 
     for doc in cursor:
         try:
-#            path = doc['filepath'].replace('/home', '/Users') + '.txt'
             with open(NEWS_FILE_PATH.format(doc['filename'])) as fp:
                 yield doc, fp.read()
         except:
-            print(NEWS_FILE_PATH.format(doc['filename']))
+            # print(NEWS_FILE_PATH.format(doc['filename']))
             pass
 
 def update_record():
@@ -29,7 +27,9 @@ def update_record():
 def add_labels(record, label, values):
     if 'labels' not in record:
         record['labels'] = {}
-    record['labels'][label] = values
+    record['labels'][label] = list(map(lambda x: { 'value': x, 'feedback': None }, values))
+
+    print(record['labels'][label])
 
 
 gen = next_record()
@@ -44,10 +44,10 @@ for doc, text in next_record():
 
     if len(countries) > 0:
         add_labels(doc, 'countries', countries)
-        update(doc)
+        # update(doc)
 
         found += 1
 
     attempts += 1
 
-print(found / attempts)
+print('Coverage: {}'.format(found / attempts))
--- static/countries.py
+++ static/countries.py
@@ -2,15 +2,32 @@
 # Key is the actual label for the country. The value is a list of other
 # words that are synonymous with the key (locations only, ideally).
 countries = {
-    'United States': ['US', 'America', 'American'],
-    'Mexico': ['MX', 'Mexico', 'Mexican'],
-    'Russia': ['USSR', 'Russian'],
-    'Canada': ['Canadian'],
-    'Brazil': ['Brazilian'],
-    'United Kingdom': ['UK', 'England'],
-    'China': ['Chinese'],
-    'India': ['Indian']
+    'Afghanistan': { 'Afghani' },
+    'Albania': { 'Albanian' },
+    'Algeria': { 'Algeria' },
+    'Andorra': { 'Andorra' },
+    'Angola': { 'Angolan' },
+    'Antigua and Barbuda': { 'Andorra' },
+    'Argentina': { 'Andorra' },
+    'Armenia': { 'Andorra' },
+    'Aruba': { 'Aruban' },
+    'Australia': { 'Australian' },
+    'Austria': { 'Austrian' },
+    'Azerbaijan': { 'Azerbaijani' },
+
+    'United States': { 'US', 'America', 'American' },
+    'Mexico': { 'MX', 'Mexico', 'Mexican' },
+    'Russia': { 'USSR', 'Russian' },
+    'Canada': { 'Canadian' },
+    'Brazil': { 'Brazilian' },
+    'United Kingdom': { 'UK', 'England' },
+    'China': { 'Chinese' },
+    'India': { 'Indian' }
 }
+
+for label, terms in countries.items():
+    countries[label].add(label)
+    countries[label] = set(map(lambda x: x.lower(), countries[label]))
 
 ## Return the countries where at least one of the synonyms (or the label itself)
 ## is found in the bag. The bag can contain single- or multi-word strings.
@@ -19,9 +36,6 @@ def find_countries(bag):
     bag = list(map(lambda x: x.lower(), bag))
 
     for label, terms in countries.items():
-        terms.append(label)
-        terms = list(map(lambda x: x.lower(), terms))
-
         if len([term for term in bag if term in terms]) > 0:
             result.append(label)
 

0a3cfc00ecb4f0f5022c13e7f72cedcb7bc8b3f8
Luke Segars <luke@lukesegars.com>
2017-02-04 10:59:29 -0500

    Added all countries recognized by the United States.
    Source: https://www.state.gov/misc/list/

--- static/countries.py
+++ static/countries.py
@@ -14,15 +14,219 @@ countries = {
     'Australia': { 'Australian' },
     'Austria': { 'Austrian' },
     'Azerbaijan': { 'Azerbaijani' },
-
-    'United States': { 'US', 'America', 'American' },
-    'Mexico': { 'MX', 'Mexico', 'Mexican' },
-    'Russia': { 'USSR', 'Russian' },
-    'Canada': { 'Canadian' },
+    
+    'The Bahamas': { 'Bahamian' },
+    'Bahrain': { },
+    'Bangladesh': { 'Bangladeshi' },
+    'Barbados': { },
+    'Belarus': { },
+    'Belgium': { 'Belgian' },
+    'Belize': { },
+    'Benin': { },
+    'Bhutan': { 'Bhutani' },
+    'Bolivia': { 'Bolivian' },
+    'Bosnia and Herzegovina': { 'Bosnia', 'Herzegovina' },
+    'Botswana': { 'Botswanan' },
     'Brazil': { 'Brazilian' },
-    'United Kingdom': { 'UK', 'England' },
+    'Brunei': { },
+    'Bulgaria': { 'Bulgarian' },
+    'Burkina Faso': { },
+    'Burma': { },
+    'Burundi': { },
+
+    'Cambodia': { 'Cambodian' },
+    'Cameroon': { 'bbb' },
+    'Canada': { 'Canadian' },
+    'Cabo Verde': { },
+    'Central African Republic': { },
+    'Chad': { },
+    'Chile': { 'Chileans' },
     'China': { 'Chinese' },
-    'India': { 'Indian' }
+    'Colombia': { 'Colombians' },
+    'Comoros': { },
+    'Democratic Republic of the Congo': { 'Congo' },
+    'Costa Rica': { 'Costa Rican' },
+    'Cote d\'Ivoire': { 'Ivory Coast' },
+    'Croatia': { 'Croatian' },
+    'Cuba': { 'Cuban' },
+    'Curacao': { },
+    'Cyprus': { },
+    'Czechia': { },
+
+    'Denmark': { 'Danish' },
+    'Djibouti': { },
+    'Dominica': { },
+    'Dominican Republic': { },
+
+    'East Timor': { 'Timor-Leste' },
+    'Ecuador': { 'Ecuadorians' },
+    'Egypt': { 'Egyptians' },
+    'El Salvador': { },
+    'Equatorial Guinea': { 'Guinea' },
+    'Eritrea': { },
+    'Estonia': { },
+    'Ethiopia': { },
+
+    'Fiji': { 'Fijian' },
+    'Finland': { },
+    'France': { 'French' },
+
+    'Gabon': { },
+    'Gambia': { },
+    'Georgia': { 'Georgian' },
+    'Germany': { 'German' },
+    'Ghana': { },
+    'Greece': { 'Greek' },
+    'Grenada': { },
+    'Guatemala': { 'Guatemalan' },
+    'Guinea': { },
+    'Guyana': { },
+
+    'Haiti': { 'Haitian' },
+    'Holy See': { 'Vatican City', 'Vatican' },
+    'Honduras': { 'Honduran' },
+    'Hong Kong': { },
+    'Hungary': { 'Hungarian' },
+
+    'Iceland': { 'Icelandic' },
+    'India': { 'Indian' },
+    'Indonesia': { 'Indonesian' },
+    'Iran': { 'Iranian' },
+    'Iraq': { 'Iraqi' },
+    'Ireland': { 'Irish' },
+    'Israel': { 'Israeli' },
+    'Italy': { 'Italian' },
+
+    'Japan': { 'Japanese' },
+    'Jamaica': { 'Jamaican' },
+    'Jordan': { 'Jordanian' },
+
+    'Kazakhstan': {  },
+    'Kenya': { 'Kenyan' },
+    'Kiribati': {  },
+    'Kosovo': {  },
+    'Kuwait': {  },
+    'Kyrgyzstan': {  },
+    
+    'Laos': { 'Laotian' },
+    'Latvia': { 'Latvian' },
+    'Lebanon': { 'Lebanese' },
+    'Lesotho': {  },
+    'Liberia': { 'Liberian' },
+    'Libya': { 'Libyan' },
+    'Liechtenstein': {  },
+    'Lithuania': { 'Lithuanian' },
+    'Luxembourg': {  },
+
+    'Macau': { 'bbb' },
+    'Macedonia': { 'bbb' },
+    'Madagascar': { 'bbb' },
+    'Malawi': { 'bbb' },
+    'Malaysia': { 'bbb' },
+    'Maldives': { 'bbb' },
+    'Mali': { 'bbb' },
+    'Malta': { 'bbb' },
+    'Marshall Islands': { 'bbb' },
+    'Mauritania': { 'bbb' },
+    'Mauritius': { 'bbb' },
+    'Mexico': { 'bbb' },
+    'Micronesia': { 'bbb' },
+    'Moldova': { 'bbb' },
+    'Monaco': { 'bbb' },
+    'Mongolia': { 'bbb' },
+    'Montenegro': { 'bbb' },
+    'Morocco': { 'bbb' },
+    'Mozambique': { 'bbb' },
+
+    'Namibia': { 'Namibian' },
+    'Nauru': {  },
+    'Nepal': { 'Nepalese' },
+    'Netherlands': { 'Dutch' },
+    'New Zealand': {  },
+    'Nicaragua': { 'Nicaraguan' },
+    'Niger': {  },
+    'Nigeria': { 'Nigerian' },
+    'North Korea': {  },
+    'Norway': { 'Norwegian' },
+
+    'Oman': {  },
+
+    'Pakistan': { 'Pakistani' }
+    'Palau': {  }
+    'Palestinian Territories': {  }
+    'Panama': { 'Panamanian' }
+    'Papau New Guinea': {  }
+    'Paraguay': {  }
+    'Peru': { 'Peruvian' }
+    'Philippines': {  }
+    'Poland': {  }
+    'Portugal': { 'Portuguese' }
+
+    'Qatar': {  }
+
+    'Romania': { 'Romanian' }
+    'Russia': { 'Russian' }
+    'Rwanda': {  }
+
+
+    'Saint Kitts and Nevis': { 'Saint Kitts' }
+    'Saint Lucia': {  }
+    'Saint Vincent and the Grenadines': { 'Saint Vincent' }
+    'Samoa': { 'Samoan' }
+    'San Marino': {  }
+    'Sao Tome and Principe': { 'Sao Tome' }
+    'Saudi Arabia': { 'Saudi Arabian' }
+    'Senegal': {  }
+    'Serbia': { 'Serbian' }
+    'Seychelles': {  }
+    'Sierra Leone': {  }
+    'Singapore': {  }
+    'Sint Maarten': {  }
+    'Slovakia': { 'Slovakian' }
+    'Slovenia': { 'Slovenia' }
+    'Solomon Islands': {  }
+    'Somalia': { 'Somalian' }
+    'South Africa': { 'South African' }
+    'South Korea': { 'South Korean' }
+    'South Sudan': { 'South Sudanese' }
+    'Spain': { 'Spanish' }
+    'Sri Lanka': { 'Sri Lankan' }
+    'Sudan': { 'Sudanese' }
+    'Suriname': {  }
+    'Swaziland': {  }
+    'Sweden': { 'Swedish' }
+    'Switzerland': { 'Swiss' }
+    'Syria': { 'Syrian' }
+
+    'Taiwan': { 'bbb' }
+    'Tajikistan': { 'bbb' }
+    'Tanzania': { 'bbb' }
+    'Thailand': { 'bbb' }
+    'Timor-Leste': { 'bbb' }
+    'Togo': {  }
+    'Tonga': {  }
+    'Trinidad and Tobago': { 'Trinidad' }
+    'Tunisia': {  }
+    'Turkey': { 'Turkish' }
+    'Turkmenistan': {  }
+    'Tuvalu': {  }
+    
+    'Uganda': { 'Ugandan' },
+    'Ukraine': { 'Ukrainian' },
+    'United Arab Emirates': {  },
+    'United Kingdom': { 'British', 'UK' },
+    'Uruguay': {  },
+    'United States': { 'US', 'America', 'American' },
+    'Uzbekistan': {  },
+
+    'Vanuatu': {  },
+    'Venezuela': { 'Venezuelan' },
+    'Vietnam': { 'Vietnamese' },
+
+    'Yemen': {  },
+
+    'Zambia': { 'Zambian' },
+    'Zimbabwe': {  },
 }
 
 for label, terms in countries.items():
